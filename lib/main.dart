fb00ca0c5309f354bbd4f14cdafff3c1419d1428
valehasadli <valehasadli@gmail.com>
2021-03-13 17:26:52 +0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'screens/shop_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Plant Shop UI',
+      debugShowCheckedModeBanner: false,
+      home: ShopScreen(),
+    );
+  }
+}
---
+++ lib/screens/shop_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plant_shop/models/plant_model.dart';
+
+class ShopScreen extends StatefulWidget {
+  @override
+  _ShopScreenState createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  PageController _pageController;
+  int _selectedPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 5,
+      vsync: this,
+    );
+
+    _pageController = PageController(
+      initialPage: 0,
+      viewportFraction: 0.8,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 30.0,
+                    color: Colors.grey,
+                  ),
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 30.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: EdgeInsets.only(left: 30.0),
+              child: Text(
+                'Top Picks',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.6),
+              labelPadding: EdgeInsets.symmetric(horizontal: 35.0),
+              isScrollable: true,
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Top',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Outdoor',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Indoor',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'New Arrivals',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Limited Edition',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              height: 500.0,
+              width: double.infinity,
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (int index) {
+                  setState(() {
+                    _selectedPage = index;
+                  });
+                },
+                itemCount: plants.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text('$index');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
