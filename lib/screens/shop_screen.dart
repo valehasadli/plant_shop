3ac3b6fb2a987c0adba0667c5c97d2c5a9fa6189
valehasadli <valehasadli@gmail.com>
2021-03-15 16:34:14 +0400

    feat: plant screen

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -137,7 +137,8 @@ class _ShopScreenState extends State<ShopScreen>
                 shape: CircleBorder(),
                 elevation: 2.0,
                 fillColor: Colors.black,
-                onPressed: () => print('add to cart'),
+                onPressed: () =>
+                    print('add to cart item id: ${plants[index].id}'),
                 child: Icon(
                   Icons.add_shopping_cart,
                   color: Colors.white,
